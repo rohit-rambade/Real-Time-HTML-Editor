c05adb7739f2eb6dec4da8def5e7e80777b8a43a
rohit <rohitrambade18@gmail.com>
2022-07-15 17:28:56 +0530

    Done But.. Need More Styling And Functionalities

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function interpreter() {
 
 //save code option
 function saveFileAs() {
-	if (promptFilename = prompt("Save file as", "")) {
-		var textBlob = new Blob([document.getElementById("editorBox").value], {type:'text/plain'});
-		var downloadLink = document.createElement("a");
-		downloadLink.download = promptFilename;
-		downloadLink.innerHTML = "Download File";
-		downloadLink.href = window.URL.createObjectURL(textBlob);
-		downloadLink.click();
-    delete downloadLink;
-    delete textBlob;
-	}
+    if (promptFilename = prompt("Save file as", "")) {
+        var textBlob = new Blob([document.getElementById("editorBox").value], { type: 'text/plain' });
+        var downloadLink = document.createElement("a");
+        downloadLink.download = promptFilename;
+        downloadLink.innerHTML = "Download File";
+        downloadLink.href = window.URL.createObjectURL(textBlob);
+        downloadLink.click();
+        delete downloadLink;
+        delete textBlob;
+    }
 }
 
 document.getElementById("save").onclick = saveFileAs;
@@ -29,8 +29,8 @@ const textarea = document.getElementById('editorBox');
 
 textarea.value = '';
 const btn = document.getElementById('reset');
-btn.addEventListener('click', function handleClick() { 
-  console.log(textarea.value);
-  textarea.value = '';
-  previewArea.innerHTML = textArea.value;
+btn.addEventListener('click', function handleClick() {
+    console.log(textarea.value);
+    textarea.value = '';
+    previewArea.innerHTML = textArea.value;
 });
